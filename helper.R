906e6d1154094cad7e682f4e93d61658873f40f7
alex <aguo@wellesley.edu>
2018-09-14 09:47:59 -0400

    renamed labels to 0 and 1, wrote function to find discrimination threshold on plot

--- helper.R
+++ helper.R
@@ -13,6 +13,8 @@ adult$fnlwgt <- NULL # final weight of response, unnecessary
 adult$relationship <- NULL # somewhat redundant, see "marital.status"
 # OMITTING ADVISED STEPS TO ORGANIZE/COMBINE LEVELS IN CERTAIN PREDICTORS
 adult$label <- adult$salary
+levels(adult$label) <- list("1" = ">50K",
+                            "0" = "<=50K")
 adult$salary <- NULL
 
 ## LOAD & CLEAN 'Pima Indians Diabetes' DATA
@@ -21,6 +23,8 @@ data("PimaIndiansDiabetes2") # has 0 values set to NA where physically impossibl
 pima <- na.omit(PimaIndiansDiabetes2) # n = 768 to n = 392
 rm(PimaIndiansDiabetes2)
 pima$label <- pima$diabetes
+levels(pima$label) <- list("1" = "pos",
+                           "0" = "neg")
 pima$diabetes <- NULL
 
 ## LOAD & CLEAN 'Sonar' DATA
@@ -28,4 +32,27 @@ data("Sonar") # has no NA values
 sonar <- Sonar
 rm(Sonar)
 sonar$label <- sonar$Class
+levels(sonar$label) <- list("1" = "M",
+                            "0" = "R")
 sonar$Class <- NULL
+
+## FUNCTION FOR FINDING SPECIFIC THRESHOLD TPR/FPR
+find_thresh <- function(test, thresh) {
+  ind <- which(test$prob >= thresh)
+  if (length(ind) != 0) {
+    lab_pos <- test$label[ind]
+    lab_neg <- test$label[-ind]
+    tp <- length(lab_pos[lab_pos=="1"])
+    tn <- length(lab_neg[lab_neg=="0"])
+    fp <- length(lab_pos[lab_pos=="0"])
+    fn <- length(lab_neg[lab_neg=="1"])
+  } else {
+    tp <- 0
+    tn <- length(test$label[test$label=="0"])
+    fp <- 0
+    fn <- length(test$label[test$label=="1"])
+  }
+  tpr <- tp / (tp + fn)
+  fpr <- fp / (fp + tn)
+  return(data.frame(FP = fpr, TP = tpr))
+}
