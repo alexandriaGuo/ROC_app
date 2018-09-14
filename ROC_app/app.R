db5763ef97efc188724b70afe49eaf2753366366
alex <aguo@wellesley.edu>
2018-09-14 09:47:01 -0400

    added discrimination threshold plotting, clear plot not working

--- ROC_app/app.R
+++ ROC_app/app.R
@@ -26,9 +26,16 @@ ui <- fluidPage (
                   choices = c("logistic",
                               "probit")
       ),
-      hr(),
       actionButton(inputId = "update",
-                   label = "Update curve")
+                   label = "Update curve"),
+      actionButton(inputId = "clear",
+                   label = "Clear plot"),
+      hr(),
+      helpText("Choose a discrimination threshold"),
+      sliderInput("thresh", NULL,
+                  min = 0, max = 1, value = 0.5),
+      actionButton(inputId = "add_pt",
+                   label = "Highlight point")
     ),
     mainPanel(
       plotOutput(outputId = "ROCplot"),
@@ -39,8 +46,12 @@ ui <- fluidPage (
 
 server <- function(input, output) {
   
-  roc <- reactiveValues(data = NULL)
+  roc <- reactiveValues(main = NULL, layer1 = NULL)
+  roc$main <- ggplot() +
+    geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1)) +
+    labs(x = '1 - Specificity', y = 'Sensitivity')
   auc <- reactiveValues(data = NULL) 
+  test_sub <- reactiveValues(data = NULL)
   
   observeEvent(input$update, {
     
@@ -51,6 +62,7 @@ server <- function(input, output) {
     model <-switch(input$model,
                    "logistic" = "logit",
                    "probit" = "probit")
+    data$label <- relevel(factor(data$label), ref = "0")
     smp_size <- floor(0.75 * nrow(data)) # 75% of the sample size
     set.seed(125) # set the seed to make partition reproducible
     train_ind <- sample(seq_len(nrow(data)), size = smp_size)
@@ -60,22 +72,30 @@ server <- function(input, output) {
     ## TEST FIT
     full.fit <- glm(label~., family=binomial(link=model), data=train)
     prob <- predict.glm(full.fit, newdata=test, type="response")
+    test_sub$data <- cbind(test, prob)
     pred <- prediction(prob, test$label)
     perf <- performance(pred, measure = "tpr", x.measure = "fpr")
     dd <- data.frame(FP = perf@x.values[[1]], TP = perf@y.values[[1]])
     
-    roc$data <- dd
+    roc$main <- roc$main + geom_line(data = dd, aes(x = FP, y = TP, colour = input$dataset))
     auc$data <- auc(test$label, prob)
   })
   
-  output$ROCplot <- renderPlot({
-    if (is.null(roc$data)) return(ggplot() + geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1)))
-    ## PLOT ROC CURVE
-    ggplot() + 
-      geom_line(data = roc$data, aes(x = FP, y = TP, color = "red")) +
+  observeEvent(input$clear, {
+    roc <- reactiveValues(main = NULL, layer1 = NULL)
+    roc$main <- ggplot() +
       geom_segment(aes(x = 0, xend = 1, y = 0, yend = 1)) +
-      guides(color=FALSE) +
-      labs(x = '1 - Specificity', y = 'Sensitivity') 
+      labs(x = '1 - Specificity', y = 'Sensitivity') +
+      guides(color=FALSE)
+  })
+  
+  observeEvent(input$add_pt, {
+    pt <- find_thresh(test_sub$data, input$thresh)
+    roc$layer1 <- geom_point(data = pt, aes(x = FP, y = TP), colour = "blue", size = 4)
+  })
+  
+  output$ROCplot <- renderPlot({
+    roc$main + roc$layer1
   })
   
   output$AUC <- renderText({
